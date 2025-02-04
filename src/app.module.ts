78cf1a32a7397c65586058afe65a581fa82b499b
Oriel Romero <orielromero97@gmail.com>
2025-02-03 21:39:31 -0300

    Setup in progress part 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { InstalationsModule } from './modules/instalations/instalations.module';
+import { CloudinaryService } from './services/cloudinary/cloudinary.service';
+import { Module } from './seeders/seeders/.module';
+import { SeedersModule } from './src/seeders/seeders.module';
+import { SeedersModule } from './seeders/seeders.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, OrdersModule, InstalationsModule, Module, SeedersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CloudinaryService],
 })
 export class AppModule {}
