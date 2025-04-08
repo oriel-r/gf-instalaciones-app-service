1cf178f045bae3abfa568d59b7d47241f1deae5c
Rocio <rociobonindev@gmail.com>
2025-04-08 19:52:49 -0300

    cambios en el modulo de instalador

--- src/modules/installer/entities/installer.entity.ts
+++ src/modules/installer/entities/installer.entity.ts
@@ -1,69 +1,79 @@
-    import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
-    import { TaxCategory } from '../../../common/enums/taxCategory.enum';
-    import { ApiProperty } from '@nestjs/swagger';
-    import { StatusInstaller } from 'src/common/enums/status-installer';
-    import { Installation } from 'src/modules/operations/installations/entities/installation.entity';
-import { UserRole } from 'src/modules/user-role/entities/user-role.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { TaxCategory } from '../../../common/enums/taxCategory.enum';
+import { ApiProperty } from '@nestjs/swagger';
+import { StatusInstaller } from 'src/common/enums/status-installer';
+import { Installation } from 'src/modules/operations/installations/entities/installation.entity';
 import { User } from 'src/modules/user/entities/user.entity';
 import { Coordinator } from 'src/modules/coordinators/entities/coordinator.entity';
 
-    @Entity({ name: 'installers' })
-    export class Installer {
-      @ApiProperty({
-        description: 'ID único del instalador.',
-        example: '123e4567-e89b-12d3-a456-426614174000',
-      })
-      @PrimaryGeneratedColumn('uuid')
-      id: string;
+@Entity({ name: 'installers' })
+export class Installer {
+  @ApiProperty({
+    description: 'ID único del instalador.',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-      @Column({
-        type: 'enum',
-        enum: TaxCategory,
-      })
-      taxCondition: TaxCategory;
+  @Column({
+    type: 'enum',
+    enum: TaxCategory,
+  })
+  taxCondition: TaxCategory;
 
-      @Column({ type: 'text', nullable: true })
-      queries?: string;
+  @Column({ type: 'text', nullable: true })
+  queries?: string;
 
-      @Column()
-      hasPersonalAccidentInsurance: boolean;
+  @Column()
+  hasPersonalAccidentInsurance: boolean;
 
-      @Column()
-      canWorkAtHeight: boolean;
+  @Column()
+  canWorkAtHeight: boolean;
 
-      @Column()
-      canTensionFrontAndBackLonas: boolean;
+  @Column()
+  canTensionFrontAndBackLonas: boolean;
 
-      @Column()
-      canInstallCorporealSigns: boolean;
+  @Column()
+  canInstallCorporealSigns: boolean;
 
-      @Column()
-      canInstallFrostedVinyl: boolean;
+  @Column()
+  canInstallFrostedVinyl: boolean;
 
-      @Column()
-      canInstallVinylOnWallsOrGlass: boolean;
+  @Column()
+  canInstallVinylOnWallsOrGlass: boolean;
 
-      @Column()
-      canDoCarWrapping: boolean;
+  @Column()
+  canDoCarWrapping: boolean;
 
-      @Column()
-      hasOwnTransportation: boolean;
+  @Column()
+  hasOwnTransportation: boolean;
 
-      @Column({
-        type: 'enum',
-        enum: StatusInstaller,
-        default: StatusInstaller.InProcess
-      })
-      status?: StatusInstaller;
+  @Column({
+    type: 'enum',
+    enum: StatusInstaller,
+    default: StatusInstaller.InProcess,
+  })
+  status?: StatusInstaller;
 
-      @OneToOne(() => User, user => user.installer)
-      @JoinColumn()
-      user: User;
+  @OneToOne(() => User, (user) => user.installer)
+  @JoinColumn()
+  user: User;
 
-      @ManyToOne(() => Coordinator, (coordinator) => coordinator.installers, { nullable: true })
-      coordinator: Coordinator;
+  @ManyToOne(() => Coordinator, (coordinator) => coordinator.installers, {
+    nullable: true,
+  })
+  coordinator: Coordinator;
 
-      @ManyToMany(() => Installation, (Installation) => Installation.installers)
-      @JoinTable()
-      installations: Installation[];
-    }
+  @ManyToMany(() => Installation, (Installation) => Installation.installers)
+  @JoinTable()
+  installations: Installation[];
+}
